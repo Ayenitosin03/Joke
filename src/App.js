5e90583f6900e3fc059d86c662865ee4173a8b0d
unknown <ayenitosin03@gmail.com>
2021-05-18 11:11:07 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,32 +4,41 @@ import './App.css'
 
 const App = () => {
   const [jokes, setJokes] = useState('');
+  const [seconds, setSeconds] = React.useState(20);
 
   const getJokes = async () => {
     try {
       const userJokes = await axios.get("https://official-joke-api.appspot.com/random_joke")
       
       setJokes(userJokes.data);
+      setSeconds(20);
     } catch (err) {
       console.error(err.message);
     }
   };
 
   useEffect( () => {
-    getJokes()
-    
-    setInterval( () => {
+    getJokes();
+  },[]);
+  
+  useEffect(()=>{
+    if(seconds > 0){
+      setTimeout(()=>setSeconds(seconds - 1),1000);
+    }
+  },[seconds]);
+  
+  useEffect(()=>{
+    if(seconds === 0){
       getJokes()
-    }, 20000)
-  }, []);
+    }
+  },[seconds])
+    
 
   return (
     <div className='jokes' key={jokes.id}>
+      <h1>Counter:{seconds}</h1>
     <h3 className='jokes__setup'>Setup: {jokes.setup}</h3>
       <p className='jokes__punchline'>Punchline: {jokes.punchline}</p>
-      <div class="balls" id="red"></div>
-      <div class="balls" id="white"></div>
-      <div class="balls" id="green"></div>
     </div>
   );
 }
